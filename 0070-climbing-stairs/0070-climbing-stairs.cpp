2d97056c0540d5bb4494b3b12a46fd1e289c98d2
shashankk0501 <1ms22is124@gmail.com>
2026-07-17 15:41:53 +0530

    Time: 0 ms (100%), Space: 7.9 MB (73.68%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int curr;
+        if(n ==0)return 0;
+        if(n==1)return 1;
+        int prev2=1;
+        int prev1=1;
+        for(int i =2;i<n+1;i++){
+            curr=prev1+prev2;
+            prev2=prev1;
+            prev1=curr;
+        }
+
+        return curr;
+    }
+};
