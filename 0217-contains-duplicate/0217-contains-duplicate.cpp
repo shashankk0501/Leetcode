41e477f955ca68f8e52c22a87e10a7c9b788fe6a
shashankk0501 <1ms22is124@gmail.com>
2026-07-22 16:58:26 +0530

    Time: 76 ms (22.6%), Space: 111.4 MB (20.01%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map <int,int> countdub;
-        int n = nums.size();
-
-        for(int i =0;i<n;i++){
-            countdub[nums[i]]++;
-            if(countdub[nums[i]]>1){
-                return true;
-            }
+        unordered_set<int> seen;
+        for(int num : nums) {
+            if(seen.count(num)) return true;
+            seen.insert(num);
         }
-
         return false;
-
     }
 };
